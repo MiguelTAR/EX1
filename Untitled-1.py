0ec4c93c3a8a741646ad5b791eca76b68af99819
MiguelTAR <mtarazona4489@conestogac.on.ca>
2022-11-22 15:05:47 -0500

    Second commit

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,47 @@
-# Name: projectcollab.py
-# Authors: Miguel Tarazona and Jaspreet Masuta
-# Date Created: 22, November, 2022
-# Date Last Modified: 22, November, 2022
-#
-# Purpose:
+def chicken(value):
+  chicken = 0
+  if (value % 10 == 0):
+        chicken = 1
 
-print("HI")
+  return chicken
+
+def fizzbuzz(value):
+    fizzbuzz = ''
+    if(value % 5 == 0 and value % 3 == 0):
+        fizzbuzz = 'fb'
+    
+    if(value % 3 == 0):
+      fizzbuzz = 'f'
+        
+
+    if(value % 5 == 0):
+      fizzbuzz = "b"
+    return fizzbuzz
+
+def display_output(mychicken, myfizzbuzz):
+  if (mychicken):
+    mychicken = 'Big fat chicken'
+  else:
+    mychicken = ''
+  if (myfizzbuzz == 'f'):
+    myfizzbuzz = "I have the fizz"
+  if (myfizzbuzz == "b"):
+    myfizzbuzz = 'I have the buzz'
+  if (myfizzbuzz == 'fb'):
+    myfizzbuzz = 'I really like the fizz and the buzz'
+    
+  buzzandchicken = '' 
+  if (mychicken and myfizzbuzz) :
+   buzzandchicken = ", and"
+     
+    
+    
+  print("{}: {}{} ".format(str(value), myfizzbuzz, buzzandchicken, mychicken))
+  
+       
+for value in range(1,16):
+  myfizzbuzz = fizzbuzz(value)
+  mychicken = chicken(value)
+  #print("{}: {} {} ".format(str(value), myfizzbuzz, mychicken))
+  display_output(mychicken, myfizzbuzz)
+
